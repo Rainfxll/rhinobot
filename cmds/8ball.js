5e1cf45bd1e33b7a1ef50f9136b2c49bd6cf15b3
Rainfxll <48831175+Rainfxll@users.noreply.github.com>
2019-09-10 20:22:49 +0200

    Update 8ball.js

--- cmds/8ball.js
+++ cmds/8ball.js
@@ -3,8 +3,8 @@
  module.exports.run = async (bot, message, args) => {
 
     //!8ball question
-    if(!args[1]) return message.reply("Plesae enter a full question with 2 or more words!");
-    let replies = ["Yes", "No", "I don't know", "Ask again later!", "Cyka", "I am not sure!", "Pls No", "You tell me", "Without a doubt", "Cannot predict now", "Without a doubt", ];
+    if(!args[1]) return message.reply("Proszę wpisz pełne pytanie z 2 lub więcej słów!");
+    let replies = ["Tak ”,„ Nie ”,„ Nie wiem ”,„ Zapytaj ponownie później ”,„ Cyka ”,„ Nie jestem pewien! ”,„ Proszę nie ”,„ Powiedz mi ”,„ Bez wątpienia ” , „Nie można teraz przewidzieć”, „Bez wątpienia", ];
 
     let result = Math.floor((Math.random() * replies.length));
     let question = args.join(" ");
