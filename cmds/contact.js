4ebbd190b8389b5372ea149dd3d7f6a31cedcbe3
Rainfxll <48831175+Rainfxll@users.noreply.github.com>
2019-09-10 18:30:44 +0200

    Update contact.js

--- cmds/contact.js
+++ cmds/contact.js
@@ -4,7 +4,7 @@ module.exports.run = async (bot, message, args) => {
   if(args[0] == "help"){
     let helpembxd = new Discord.RichEmbed()
     .setColor("#00ff00")
-    .addField("Contact Command", "Usage: !Contact <reason>")
+    .addField("Kontakt do supportu", "Usage: !contact <powód>")
 
     message.channel.send(helpembxd);
     return;
@@ -13,26 +13,26 @@ module.exports.run = async (bot, message, args) => {
     let Invite = await message.guild.channels.find((c) => c.type === 'text').createInvite()
     let Sender = message.author;
     const sayMessage = args.join(" ");
-    if(!sayMessage) return message.channel.send("Please give us reason for contacting").then(msg => {msg.delete(5000)});
+    if(!sayMessage) return message.channel.send("Proszę uzasadnić swój problem!").then(msg => {msg.delete(5000)});
 
    let contact = new Discord.RichEmbed()
    .setColor("00ff00")
    .setThumbnail(Sender.displayAvatarURL)
-   .setDescription(`Contact message from [${message.guild.name}](${Invite.url})`)
-   .setTitle("Message from contact command!")
-   .addField("User", Sender, true)
-   .addField("User ID: ", Sender.id, true)
-   .addField("Message: ", sayMessage)
+   .setDescription(`Kontakt z serwera [${message.guild.name}](${Invite.url})`)
+   .setTitle("Rhino BOT - Support")
+   .addField("uZYTKOWNIK", Sender, true)
+   .addField("ID uŻYTKOWNIKA: ", Sender.id, true)
+   .addField("Wiadomość: ", sayMessage)
    .setTimestamp()
 
     bot.users.get("461215237002231808").send(contact);
 
     let embed = new Discord.RichEmbed()
     .setColor("#00ff00")
-    .setTitle("Message Sent!")
+    .setTitle("Rhino BOT Support")
     .setDescription("Twoja wiadomość została wysłana")
-    .addField("Reqested by ", Sender)
-    .addField("Message: ", sayMessage)
+    .addField("Wysłał ", Sender)
+    .addField("Wiadomość: ", sayMessage)
     .setFooter("Dziękujemy za wysłanie wiadomości do supportu Rhino BOT")
 
     message.channel.send(embed).then(msg => {msg.delete(10000)});
