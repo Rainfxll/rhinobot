0da53e5af034f54503bf0c141352596ad54195d3
Rainfxll <48831175+Rainfxll@users.noreply.github.com>
2019-09-10 19:24:01 +0200

    Update warn.js

--- cmds/warn.js
+++ cmds/warn.js
@@ -2,24 +2,24 @@ const Discord = require('discord.js');
 const fs = require("fs");
 
 exports.run = (client, message, args) => {
-  if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("You don't have premission to do that!");
+  if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("Nie możesz użyć tej komendy!");
   let reason = args.slice(1).join(' ');
   let user = message.mentions.users.first();
-  if (message.mentions.users.size < 1) return message.reply('You must mention someone to warn them.');
-  if (reason.length < 1) return message.reply('You must have a reason for the warning.');
+  if (message.mentions.users.size < 1) return message.reply('Musisz wpisać nazwę aby kogoś upomnieć!');
+  if (reason.length < 1) return message.reply('Musisz wpisać powód...');
 
   let dmsEmbed = new Discord.RichEmbed()
-  .setTitle("Warn")
+  .setTitle("Rhino BOT - Ostrzeżenie")
   .setColor("#00ff00")
-  .setDescription(`You have been warned on \`${message.guild.name}\``)
-  .addField("Warned by", message.author.tag)
-  .addField("Reason", reason);
+  .setDescription(`Zostałeś ostrzeżony \`${message.guild.name}\``)
+  .addField("Ostrzeżony przez", message.author.tag)
+  .addField("Powód", reason);
 
   user.send(dmsEmbed);
 
   message.delete();
   
-  message.channel.send(`${user.tag} has been warned`)
+  message.channel.send(`${user.tag} Został ostrzeżony`)
 
 }
 
