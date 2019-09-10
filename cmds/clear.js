41bc40d5fbc72988fa8c862b3aa1a065d06870fd
Rainfxll <48831175+Rainfxll@users.noreply.github.com>
2019-09-10 19:10:54 +0200

    Update clear.js

--- cmds/clear.js
+++ cmds/clear.js
@@ -12,10 +12,10 @@ module.exports.run = async (bot, message, args) => {
 
   message.delete()
 
-  if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("You don't have premssions to do that!");
-  if(!args[0]) return message.channel.send("Please enter a number of messages to clear! `Usage: !clear <amount>`");
+    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("Nie możesz użyć tej komendy!");
+  if(!args[0]) return message.channel.send("Proszę wpisać liczbę! Użycie r!clear <liczba>`");
   message.channel.bulkDelete(args[0]).then(() => {
-  message.channel.send(`**__Cleared ${args[0]} messages.__**`).then(msg => msg.delete(2000));
+  message.channel.send(`**__Usunięto ${args[0]} wiadomości.__**`).then(msg => msg.delete(2000));
 });
 
 
