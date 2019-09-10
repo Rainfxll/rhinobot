e640dfc0d529e307afcc2aff8c1e1c96904efb3d
Rainfxll <48831175+Rainfxll@users.noreply.github.com>
2019-09-10 20:30:36 +0200

    Update avatar.js

--- cmds/avatar.js
+++ cmds/avatar.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-    let msg = await message.channel.send("Generating avatar...");
+    let msg = await message.channel.send("Generowanie avataru...");
 
     let mentionedUser = message.mentions.users.first() || message.author;
 
@@ -9,8 +9,8 @@ module.exports.run = async (bot, message, args) => {
 
         .setImage(mentionedUser.displayAvatarURL)
         .setColor("00ff00")
-        .setTitle("Avatar")
-        .setFooter("Searched by " + message.author.tag)
+        .setTitle("Rhino BOT -  AVATAR")
+        .setFooter("Wyszukane przez: " + message.author.tag)
         .setDescription("[Avatar URL link]("+mentionedUser.displayAvatarURL+")");
 
         message.channel.send(embed)
