3060f3caa70f6d4c5b2634ca66eeb8216473d6d7
Rainfxll <48831175+Rainfxll@users.noreply.github.com>
2019-11-23 21:33:27 +0100

    Update mute.js

--- cmds/mute.js
+++ cmds/mute.js
@@ -6,16 +6,16 @@ module.exports.run = async (bot, message, args) => {
   //!mute @user 1s/m/h/d
 
   let tomute = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-  if(!tomute) return message.channel.send("Please tag user to mute!");
-  if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("Sorry, you don't have permissions to use this!");
-  if(tomute.hasPermission("MANAGE_MESSAGES")) return message.channel.send("I cant mute this user");
-  if (tomute.id === message.author.id) return message.channel.send("You cannot mute yourself!");
-  let muterole = message.guild.roles.find(`name`, "Odar Mute");
+  if(!tomute) return message.channel.send("Oznacz użytkownika, aby go wyciszyć!");
+  if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("Przepraszamy, nie masz uprawnień do korzystania z tego!");
+  if(tomute.hasPermission("MANAGE_MESSAGES")) return message.channel.send("Nie mogę wyciszyć tego użytkownika");
+  if (tomute.id === message.author.id) return message.channel.send("Nie możesz się wyciszyć!");
+  let muterole = message.guild.roles.find(`name`, "» | Kara: Mute");
 
   if(!muterole){
     try{
       muterole = await message.guild.createRole({
-        name: "Odar Mute",
+        name: "» | Kara: Mute",
         color: "#000000",
         permissions:[]
       })
@@ -31,14 +31,14 @@ module.exports.run = async (bot, message, args) => {
   }
 
   let mutetime = args[1];
-  if(!mutetime) return message.channel.send("You didn't specify a time!");
+  if(!mutetime) return message.channel.send("Nie podałeś czasu!");
 
   await(tomute.addRole(muterole.id));
-  message.reply(`<@${tomute.id}> has been muted for ${ms(ms(mutetime))}`);
+  message.reply(`<@${tomute.id}> został wyciszony na ${ms(ms(mutetime))}`);
 
   setTimeout(function(){
     tomute.removeRole(muterole.id);
-    message.channel.send(`<@${tomute.id}> has been unmuted!`);
+    message.channel.send(`<@${tomute.id}> został wyciszony!`);
   }, ms(mutetime));
 
   message.delete();
