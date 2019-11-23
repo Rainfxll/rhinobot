1fcb6b1e17fc3a2530a50e378bd62d6728c32090
Rainfxll <48831175+Rainfxll@users.noreply.github.com>
2019-11-23 22:59:05 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,6 +8,46 @@ const prefix = botSettings.prefix;
 const bot = new Discord.Client({disableEveryone: true});
 bot.commands = new Discord.Collection();
 
+//    Add emoji name
+var emojiname = ["",""];
+
+//    Add role name
+var rolename=["",""];
+
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}`);
+});
+
+
+
+client.on('message', msg => {
+
+if(msg.content.startsWith(prefix+"reaction")){
+  if(!msg.channel.guild) return;
+  for(let n in emojiname){
+  var emoji =[msg.guild.emojis.find(r => r.name == emojiname[n])];
+  for(let i in emoji){
+   msg.react(emoji[i]);
+  }
+ }
+}
+});
+
+
+
+client.on("messageReactionAdd",(reaction,user)=>{
+  if(!user) return;
+  if(user.bot)return;
+  if(!reaction.message.channel.guild) return;
+  for(let n in emojiname){
+  if(reaction.emoji.name == emojiname[n]){
+    let role = reaction.message.guild.roles.find(r => r.name == rolename[n]);          
+    reaction.message.guild.member(user).addRole(role).catch(console.error);
+  }
+}
+});
+
 fs.readdir("./cmds", (err, files) => {
     if(err) console.error(err);
 
