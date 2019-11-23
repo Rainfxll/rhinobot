870c7eb7143ae16a25e4479bb8d830aa42cec825
Rainfxll <48831175+Rainfxll@users.noreply.github.com>
2019-11-23 21:51:05 +0100

    Update unmute.js

--- cmds/unmute.js
+++ cmds/unmute.js
@@ -1,15 +1,15 @@
 module.exports.run = async (bot, message, args) => {
-    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.sendMessage("You don't have the `Manage Messages` premission")
+    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.sendMessage("Nie masz premii `Zarządzaj wiadomościami '")
 
         let toMute = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
-        if(!toMute) return message.channel.sendMessage("Please mention an user or ID to mute!");
+        if(!toMute) return message.channel.sendMessage("Podaj użytkownika lub identyfikator, aby wyciszyć!");
 
-        let role = message.guild.roles.find(r => r.name === "Odar Mute")
+        let role = message.guild.roles.find(r => r.name === "» | Kara: Mute")
         
         if(!role || !toMute.roles.has(role.id)) return message.channel.sendMessage("This user is not muted!");
 
         await toMute.removeRole(role);
-        message.channel.sendMessage("The user has been unmuted!");
+        message.channel.sendMessage("Użytkownik nie został wyciszony!");
 
         message.delete();
 
