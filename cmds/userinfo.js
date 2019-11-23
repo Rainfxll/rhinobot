1194b89a28cc9d74f722a4e079e156110f042a0f
Rainfxll <48831175+Rainfxll@users.noreply.github.com>
2019-11-23 22:00:59 +0100

    Update userinfo.js

--- cmds/userinfo.js
+++ cmds/userinfo.js
@@ -4,34 +4,34 @@ module.exports.run =async (bot, message, args) => {
     let inline = true
     let resence = true
     const status = {
-        online: "<:online:424890369688469504> Online",
-        idle: "<:idle:424890472855502849> Idle",
-        dnd: "<:dnd:424890429524410368> Do Not Disturb",
-        offline: "<:offilne:424890400319340546> Offline/Invisible"
+        online: "<:white_check_mark: 424890369688469504> Online",
+        idle: "<:back:424890472855502849> Zaraz Wracam",
+        dnd: "<:shield: 424890429524410368> Nie przeszkadzać",
+        offline: "<:x:424890400319340546> Offline/Nie widoczny"
       }
         
 const member = message.mentions.members.first() || message.guild.members.get(args[0]) || message.member;
 let target = message.mentions.users.first() || message.author
 
 if (member.user.bot === true) {
-    bot = "<:bottag:425631858265423883> Yes";
+    bot = "<:busts_in_silhouette: 425631858265423883> Tak";
   } else {
-    bot = "<:user:424958082691629057> No";
+    bot = "<:bust_in_silhouette:424958082691629057> Nie";
   }
 
             let embed = new Discord.RichEmbed()
                 //.setAuthor(member.user.username)
                 .setThumbnail((target.displayAvatarURL))
                 .setColor("#00ff00")
-                .addField("Full Username", `${member.user.tag}`, inline)
+                .addField("Pełna Nazwa", `${member.user.tag}`, inline)
                 .addField("ID", member.user.id, inline)
-                .addField("Nickname", `${member.nickname !== null ? `<:yes:425632265993846795> Nickname: ${member.nickname}` : "<:no:425632070036094986> None"}`, true)
+                .addField("Nazwa", `${member.nickname !== null ? `<:yes:425632265993846795> Nickname: ${member.nickname}` : "<:no:425632070036094986> None"}`, true)
                 .addField("Bot", `${bot}`,inline, true)
                 .addField("Status", `${status[member.user.presence.status]}`, inline, true)
-                .addField("Playing", `${member.user.presence.game ? `🎮 ${member.user.presence.game.name}` : "<:no:425632070036094986> Not playing"}`,inline, true)
-                .addField("Roles", `${member.roles.filter(r => r.id !== message.guild.id).map(roles => `\`${roles.name}\``).join(" **|** ") || "<:no:425632070036094986> No Roles"}`, true)
-                .addField("Joined Discord At", member.user.createdAt)
-                .setFooter(`Information about ${member.user.username}`)
+                .addField("Gra", `${member.user.presence.game ? `🎮 ${member.user.presence.game.name}` : "<:x:425632070036094986> Nie Gra"}`,inline, true)
+                .addField("Role", `${member.roles.filter(r => r.id !== message.guild.id).map(roles => `\`${roles.name}\``).join(" **|** ") || "<:no:425632070036094986> No Roles"}`, true)
+                .addField("Dołączył do discorda", member.user.createdAt)
+                .setFooter(`Informacje o ${member.user.username}`)
                 .setTimestamp()
     
             message.channel.send(embed);
