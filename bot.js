7c0f745835e299859657212ea758360fe8d97574
Rainfxll <48831175+Rainfxll@users.noreply.github.com>
2019-11-23 23:36:40 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,46 +8,15 @@ const prefix = botSettings.prefix;
 const bot = new Discord.Client({disableEveryone: true});
 bot.commands = new Discord.Collection();
 
-client.on('messageReactionAdd', async (reaction, user) => {
-    
-    let applyRole = async () => {
-        let emojiName = reaction.emoji.name;
-        let role = reaction.message.guild.roles.find(role => role.name.toLowerCase() === emojiName.toLowerCase());
-        let member = reaction.message.guild.members.find(member => member.id === user.id);
-        try {
-            if(role && member) {
-                console.log("Role and member found.");
-                await member.roles.add(role);
-                console.log("Done.");
-            }
-        }
-        catch(err) {
-            console.log(err);
-        }
-    }
-    if(reaction.message.partial)
-    {
-        try {
-            let msg = await reaction.message.fetch(); 
-            console.log(msg.id);
-            if(msg.id === '647922766166753290')
-            {
-                console.log("Cached")
-                applyRole();
-            }
-        }
-        catch(err) {
-            console.log(err);
-        }
-    }
-    else 
-    {
-        console.log("Not a partial.");
-        if(reaction.message.id === '647922766166753290') {
-            console.log(true);
-            applyRole();
-        }
-    }
+bot.on('messageReactionAdd', (messageReaction, user) => {
+if(user.bot)  return;
+const { message, emoji } = messageReaction;
+
+if(emoji.name === ":white_check_mark: ") {
+if(message.id === "647922766166753290") {
+// code to run when that emoji is reacted on specified message
+  }
+ } 
 });
 
 fs.readdir("./cmds", (err, files) => {
