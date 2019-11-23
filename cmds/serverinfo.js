0a34e103a340319ffeb8deccaf5fd58e865242db
Rainfxll <48831175+Rainfxll@users.noreply.github.com>
2019-11-23 21:43:38 +0100

    Update serverinfo.js

--- cmds/serverinfo.js
+++ cmds/serverinfo.js
@@ -15,16 +15,16 @@ module.exports.run = async (bot, message, args) => {
     .setColor("#00ff00")
     .setThumbnail(sicon)
     .setAuthor(message.guild.name)
-    .addField("Name", message.guild.name, inline)
+    .addField("Nazwa", message.guild.name, inline)
     .addField("ID", message.guild.id, inline)
-    .addField("Owner", message.guild.owner, inline)
+    .addField("Założyciel", message.guild.owner, inline)
     .addField("Region", message.guild.region, inline)
-    .addField("Verification Level", verlvl[message.guild.verificationLevel],inline)
-    .addField("Members", `<:user:424958082691629057> ${message.guild.memberCount}`, inline)
-    .addField("Roles", message.guild.roles.size, inline)
-    .addField("Channels", message.guild.channels.size, inline)
-    .addField("You Joined", message.member.joinedAt)
-    .setFooter(`Created ${message.guild.createdAt}`);
+    .addField("Level Weryfikacji", verlvl[message.guild.verificationLevel],inline)
+    .addField("Użytkownicy", `<:user:424958082691629057> ${message.guild.memberCount}`, inline)
+    .addField("Role", message.guild.roles.size, inline)
+    .addField("Kanały", message.guild.channels.size, inline)
+    .addField("Dołączyłeś", message.member.joinedAt)
+    .setFooter(`Utworzono ${message.guild.createdAt}`);
 
     message.channel.send(serverembed);
 
