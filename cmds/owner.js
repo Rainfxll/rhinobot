be148871291c27c984a7d63665caa8e779173154
Rainfxll <48831175+Rainfxll@users.noreply.github.com>
2019-11-23 21:35:20 +0100

    Update owner.js

--- cmds/owner.js
+++ cmds/owner.js
@@ -3,9 +3,9 @@ const Discord = module.require("discord.js")
 module.exports.run = async (bot, message, args) => {
 
     let owner = message.mentions.members.first() || message.guild.members.get(args[0]);
-    if(!owner) return message.channel.send("Please tag someone so i can make them the new Server Owner");
+    if(!owner) return message.channel.send("Proszę oznaczyć kogoś, aby mógł zostać nowym właścicielem serwera");
 
-    message.channel.send(`Made ${owner} the Server Owner successfully! ✅`)
+    message.channel.send(`Udało się zostać ${owner} nowym właścicielem serwera ✅`)
 
 
 }        
