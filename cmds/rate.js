c3324ef1ebc70ba5e7601e880f6d04595159926b
Rainfxll <48831175+Rainfxll@users.noreply.github.com>
2019-11-23 21:37:35 +0100

    Update rate.js

--- cmds/rate.js
+++ cmds/rate.js
@@ -3,15 +3,15 @@ const Discord = require("discord.js")
 module.exports.run = async (bot, message, args) => {
 
 let ratus = message.mentions.members.first();
-if(!ratus) return message.channel.send("Tag someone to rate them!");
+if(!ratus) return message.channel.send("Oznacz kogoś, aby go ocenić!");
 
 let rates = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
 
 let result = Math.floor((Math.random() * rates.length));
 
 if(ratus.user.id === message.author.id) {
-  return message.channel.send(`**${message.author.username}**, I'd give you ${result}/10<:thonk:427846193503272960>`);
-} else return message.channel.send(`I'd give **__${ratus.user.username}__** ${result}/10 <:thonk:427846193503272960>`);
+  return message.channel.send(`**${message.author.username}**, Dałbym ci ${result}/10<:thought_balloon: 427846193503272960>`);
+} else return message.channel.send(`Dałbym ci **__${ratus.user.username}__** ${result}/10 <thought_balloon427846193503272960>`);
 
 }
 module.exports.help = {
