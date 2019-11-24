6e35f2d2146e402539a80be76ad2bd8118f7c7e7
Rainfxll <48831175+Rainfxll@users.noreply.github.com>
2019-11-24 14:09:01 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
 const botSettings = require("./botsettings.json")
-codnst Discord = require("discord.js");
+const Discord = require("discord.js");
 const fs = require("fs");
 const ytdl = require("ytdl-core");
 const request = require("request");
