748ca62533d2e381e64da0d13d143216ba6daa9e
Rainfxll <48831175+Rainfxll@users.noreply.github.com>
2020-01-16 09:07:26 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -26,8 +26,8 @@ try {
 setInterval(function() {
   console.log('Getting stats update..')
   
-  bot.channels.get("663170416788897793").setName(`» | Serwery: ${bot.guilds.size}/2`)
-  bot.channels.get("663170593180352512").setName(`» | Użytkownicy: ${bot.users.size}`)
+  on.channels.get("663170416788897793").setName(`» | Serwery: ${bot.guilds.size}/2`)
+  on.channels.get("663170593180352512").setName(`» | Użytkownicy: ${bot.users.size}`)
   }, 30000)
 
 bot.on("message", async message => {
